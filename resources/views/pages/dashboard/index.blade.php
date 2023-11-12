3a743e7406c4c9128fb5298b85e8f998d5e2e814
HusniArr <husniarrasyid@gmail.com>
2023-11-12 15:26:07 +0700

    pasang library jquery-ui

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <!-- Sale & Revenue Start -->
     <div class="container-fluid pt-4 px-4">
-        <h6 class="mt-2 mb-2">Welcome to Dashboard , {{ strtoupper($user['nm_ptg']) }}. </h6 class="mt-2 mb-2">
+        <h6 class="mt-2 mb-2">Selamat datang di Dashboard , {{ strtoupper($user['nm_ptg']) }}. </h6 class="mt-2 mb-2">
         <div class="row g-4">
             <div class="col-sm-6 col-xl-3">
                 <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
                     <i class="fa fa-chart-line fa-3x text-primary"></i>
                     <div class="ms-3">
-                        <p class="mb-2">Today Sale</p>
-                        <h6 class="mb-0">$1234</h6>
+                        <p class="mb-2">Penjualan per hari</p>
+                        <h6 class="mb-0">Rp. {{ number_format($total_today_sale, 0, ',', '.')  }}</h6>
                     </div>
                 </div>
             </div>
@@ -18,26 +18,26 @@
                 <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
                     <i class="fa fa-chart-bar fa-3x text-primary"></i>
                     <div class="ms-3">
-                        <p class="mb-2">Total Sale</p>
-                        <h6 class="mb-0">$1234</h6>
+                        <p class="mb-2">Semua Penjualan</p>
+                        <h6 class="mb-0">Rp. {{ number_format($total_all_sales, 0, ',', '.')  }}</h6>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-xl-3">
                 <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-chart-area fa-3x text-primary"></i>
+                    <i class="fas fa-box fa-3x text-primary"></i>
                     <div class="ms-3">
-                        <p class="mb-2">Today Revenue</p>
-                        <h6 class="mb-0">$1234</h6>
+                        <p class="mb-2">Total Item</p>
+                        <h6 class="mb-0">{{ $total_items }}</h6>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-xl-3">
                 <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-chart-pie fa-3x text-primary"></i>
+                    <i class="fas fa-box-open fa-3x text-primary"></i>
                     <div class="ms-3">
-                        <p class="mb-2">Total Revenue</p>
-                        <h6 class="mb-0">$1234</h6>
+                        <p class="mb-2">Total Item terjual</p>
+                        <h6 class="mb-0">{{ $total_item_sale }}</h6>
                     </div>
                 </div>
             </div>
